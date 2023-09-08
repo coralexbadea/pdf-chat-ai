fe3abcc2442a04a8a8c4316386578ffa61ac2aaf
coralexbadea <coralexbadea99@gmail.com>
2023-09-08 19:23:47 +0300

    ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
     <main className="relative container flex min-h-screen flex-col">
       <div className=" p-4 flex h-14 items-center justify-between supports-backdrop-blur:bg-background/60 sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur">
         <span className="font-bold">DorelAI v1.0</span>
+        <span className="font-italic">Eu sunt Dorel, asistentul tau inteligent. Am cunostinte despre legile date in 2023, intreaba-ma orice.🤖</span>
         <DarkModeToggle />
       </div>
       <div className="flex flex-1 py-4">
